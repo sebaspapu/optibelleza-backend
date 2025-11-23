13444e1398dc3210b6e895be5e3fc85da7d62156
sebaspapu <sebasnarutoxd@gmail.com>
2025-11-23 03:04:20 -0500

    refactor: change in schemas
    se realiza un cambio en la parte de schemas, de modo que ahora es posible obtener los productos en el front sin problemas, teniendo en cuenta la estructura propuesta

--- app/models/product.py
+++ app/models/product.py
@@ -4,15 +4,15 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy import text
 from sqlalchemy.orm import relationship
 
-class Product(Base):
-    __tablename__="product"
+class Shoes(Base):
+    __tablename__="shoes"
     id=Column(Integer,primary_key=True,nullable=False)
     name=Column(String,nullable=False)
     price=Column(Integer,nullable=False)
-    product_mounts_type=Column(String,nullable=False)
+    shoes_type=Column(String,nullable=False)
     product_image=Column(String,nullable=False)
-    product_mounts_category=Column(String,nullable=False)
-    product_mounts_stock=Column(Integer,server_default=text('0'))
-    product_mounts_description=Column(String,server_default=text("''"))
+    shoes_category=Column(String,nullable=False)
+    shoes_stock=Column(Integer,server_default=text('0'))
+    shoes_description=Column(String,server_default=text("''"))
     created_at=Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('CURRENT_TIMESTAMP'))
     
